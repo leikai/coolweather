1d8d1bb012e9d37a475a9d47202325113489e3a4
lk <18335182938@163.com>
2018-07-18 15:39:39 +0800

    郭神coolWeather手敲版

---
+++ app/src/main/java/com/bs/lk/coolweather/LApplication.java
@@ -0,0 +1,10 @@
+package com.bs.lk.coolweather;
+
+import org.litepal.LitePalApplication;
+
+public class LApplication extends LitePalApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
